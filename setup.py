c03c18a7b1d7570c34089b31b8d96d27f4281b03
akur <ajkurzawski@gmail.com>
2017-04-04 15:56:32 -0500

    Change version in setup to first working version.

--- setup.py
+++ setup.py
@@ -2,8 +2,10 @@ from distutils.core import setup
 
 setup(
     name='hyperamoeba',
-    version='0.1dev',
-    packages=['hyperamoeba',],
+    version='0.1',
+    url='https://github.com/ajkur/hyperamoeba',
+    author='Andrew Kurzawski',
+    packages=['hyperamoeba'],
     license='LICENSE.txt',
     long_description=open('README.md').read(),
 )
